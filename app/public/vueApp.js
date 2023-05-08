93442a286639b90a77ce50cb91716245193a1be2
Mauro Farina <maurofarina@protonmail.com>
2023-05-08 16:05:51 +0200

    mount datasetFolder

--- app/public/vueApp.js
+++ app/public/vueApp.js
@@ -1,5 +1,6 @@
 import {ApiDocs} from './vueComponents/apiDocs.js';
 import {DatasetDocs} from './vueComponents/datasetDocs.js';
+import {DatasetFolder} from './vueComponents/datasetFolder.js';
 import {DatabaseQueryInterface} from './vueComponents/dbQueryInterface.js';
 import {GeneralInfoQueryInterface} from './vueComponents/generalInfoQueryInterface.js';
 import {RaceInfoQueryInterface} from './vueComponents/raceInfoQueryInterface.js';4
@@ -28,10 +29,12 @@ const app = createApp({
 
 app.component("ApiDocs", ApiDocs);
 app.component("DatasetDocs", DatasetDocs);
+app.component("DatasetFolder", DatasetFolder);
 app.component("DatabaseQueryInterface", DatabaseQueryInterface);
 app.component("GeneralInfoQueryInterface", GeneralInfoQueryInterface);
 app.component("RaceInfoQueryInterface", RaceInfoQueryInterface);
 app.component("RaceLapTimesQueryInterface", RaceLapTimesQueryInterface);
 app.component("ChampionshipBattleChart", ChampionshipBattleChart);
+
 
 app.mount("#app");
