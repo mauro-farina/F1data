eec334a95f4454610f24e0a33acdfb3ce2740749
Mauro Farina <maurofarina@protonmail.com>
2023-05-08 15:41:28 +0200

    fix oopsie

--- app/mongodb.js
+++ app/mongodb.js
@@ -1,8 +1,6 @@
 const {MongoClient} = require("mongodb");
 require("dotenv").config({ path: './private/settings.env' });
 
-//const connectionString = "mongodb+srv://maurofarina99:op3n-d4ta-mngmnT-c1oud-f1@f1app.kjpwvk7.mongodb.net/test";
-//console.log(process.env.MONGODB_URI);
 const client = new MongoClient(process.env.MONGODB_URI);
 
 let _db;
