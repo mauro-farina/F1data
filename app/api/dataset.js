f675b176d6e2851099440ff7f2498c5110ffcfe0
Mauro Farina <maurofarina@protonmail.com>
2023-05-08 19:17:45 +0200

    add dublin core metadata to dataset http responses

--- app/api/dataset.js
+++ app/api/dataset.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 
 const DATASET_DIRECTORY = __dirname + '/../../f1data/';
+const DOMAIN = 'https://f1-app-demo-a5kvrhkn4a-ew.a.run.app';
 
 router.get('/dataset', async (req, res) => {
     try {
@@ -16,6 +17,15 @@ router.get('/dataset', async (req, res) => {
                 last_modified : fileInfo.mtime
             });
         }
+        res.set('dc.title', 'F1 Dataset');
+        res.set('dc.creator', 'Mauro Farina');
+        res.set('dc.subject', 'F1, Formula 1, Formula One, Motorsport, csv, api, dataset');
+        res.set('dc.description', 'A dataset containing information about the latest seasons of Formula 1');
+        res.set('dc.type', 'json');
+        res.set('dc.format', 'application/json');
+        res.set('dc.identifier', DOMAIN + '/api/dataset');
+        res.set('dc.language', 'EN');
+        res.set('dc.rights', 'CCO 1.0 Universal');
         res.send(response);
     } catch(err) {
         console.error(err);
@@ -33,6 +43,15 @@ router.get('/dataset/:filename', async (req, res) => {
             res.status(500).send('File not found.');
             return;
         }
+        res.set('dc.title', req.params.filename);
+        res.set('dc.creator', 'Mauro Farina');
+        res.set('dc.subject', 'F1, Formula 1, Motorsport, csv, api, dataset');
+        res.set('dc.description', 'A dataset containing information about the latest seasons of Formula 1');
+        res.set('dc.type', 'csv');
+        res.set('dc.format', 'text/csv');
+        res.set('dc.identifier', DOMAIN + '/api/dataset/' + req.params.filename);
+        res.set('dc.language', 'EN');
+        res.set('dc.rights', 'CCO 1.0 Universal');
         res.set('Content-Type', 'text/csv');
         res.send(data);
     });
